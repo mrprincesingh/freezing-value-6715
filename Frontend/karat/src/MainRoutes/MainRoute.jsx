0147ddc8733ae061f23cacfc72c65098a2c23525
mrprincesinghhh <princeranu98@gmail.com>
2023-02-25 22:36:49 +0530

    admin

--- Frontend/karat/src/MainRoutes/MainRoute.jsx
+++ Frontend/karat/src/MainRoutes/MainRoute.jsx
@@ -1,30 +1,43 @@
-import React from "react";
-import { Routes, Route } from "react-router-dom";
-import Home from "../Pages/Home";
-import Cart from "../Pages/Cart";
-import Checkout from "../Pages/Checkout";
-import Login from "../Pages/Login";
-import SignUp from "../Pages/SignUp";
-import Payment from "../Pages/Payment";
-import Products from "../Pages/Products";
-import SingleProduct from "../Pages/SingleProduct";
-import PrivateRoute from "./PrivateRoute";
+import React from 'react'
+import {Routes , Route} from "react-router-dom"
+import Home from '../Pages/Home'
+import Cart from "../Pages/Cart"
+import Checkout from "../Pages/Checkout"
+import Login from "../Pages/Login"
+import SignUp from "../Pages/SignUp"
+import Payment from "../Pages/Payment"
+import Products from "../Pages/Products"
+import SingleProduct from "../Pages/SingleProduct"
+import Admin from '../Admin/Admin'
+import AdminEdit from '../Admin/AdminEdit'
+import AddProduct from '../Admin/AddProduct'
+import Order from '../Admin/Order'
+import User from '../Admin/User'
+import AdminLogin from '../Admin/AdminLogin'
+
 
 const MainRoute = () => {
   return (
     <div>
         <Routes>
           <Route path="/" element={<Home/>} />
+          <Route path="/admin" element={<Admin/>}/>
+          <Route path="/adminedit/:id" element={<AdminEdit/>}/>
+          <Route path="/addproduct" element={<AddProduct/>} />
+          <Route path="/orders" element={<Order />}/>
+          <Route path="/admin/users" element={<User/>}/>
+          <Route path="/admin/login" element={<AdminLogin/>}/>
+          <Route path="/cart" element={<Cart/>} />
           <Route path="/checkout" element={<Checkout/>} />
           <Route path="/login" element={<Login/>} />
           <Route path="/signup" element={<SignUp/>} />
           <Route path="/payment" element={<Payment/>} />
           <Route path="/product" element={<Products/>} />
-          <Route path="/product/:_id" element={<SingleProduct/>} />
-          <Route path="/cart" element={<Cart/>} />
+          <Route path="/product/:id" element={<SingleProduct/>} />
+
         </Routes>
     </div>
-  );
-};
+  )
+}
 
-export default MainRoute;
+export default MainRoute
